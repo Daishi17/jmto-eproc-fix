780ad2433abb3559905759ef93a6ecb7a7e395d4
Pramsco <anggapramuja0000@gmail.com>
2024-01-23 11:44:29 +0700

    'update'

--- application/controllers/panitia/daftar_paket/Daftar_paket.php
+++ application/controllers/panitia/daftar_paket/Daftar_paket.php
@@ -437,7 +437,7 @@ class Daftar_paket extends CI_Controller
 		Nama Paket: ' . $data_rup['nama_rup'] . ' 
 		Jenis Pengadaan: ' . $data_rup['nama_jenis_pengadaan']  . '
 		Silahkan Mengikuti Melalui Link Ini : https://drtproc.jmto.co.id/auth 
-		Selambat-Lambatnya Pada : 
+		Selambat-Lambatnya Pada : ' . $data_rup['batas_pendaftaran_tender']  . '
 		Terimakasih');
 		foreach ($get_panitia_terpilih as $key => $value2) {
 			if ($value2['role_panitia'] == 1) {
