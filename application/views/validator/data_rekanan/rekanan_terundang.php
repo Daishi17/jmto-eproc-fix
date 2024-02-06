4ac60f1bc3d650af149c13edfd6c2b3893158fcb
unknown <danang1724@gmail.com>
2024-02-06 13:44:27 +0700

    update

--- application/views/validator/data_rekanan/rekanan_terundang.php
+++ application/views/validator/data_rekanan/rekanan_terundang.php
@@ -105,7 +105,7 @@
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header bg-success">
-                <h5 class="modal-title text-white">Kirim Pesan</h5>
+                <h5 class="modal-title text-white">Kirim Pesan <label for="" id="nama_usaha_pesan"></label></h5>
                 <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
