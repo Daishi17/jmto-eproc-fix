25f2fc6f07bdff581d6396a4dfa0f9fb85119292
unknown <danang1724@gmail.com>
2024-02-27 09:53:05 +0700

    update

--- application/controllers/panitia/daftar_paket/Daftar_paket.php
+++ application/controllers/panitia/daftar_paket/Daftar_paket.php
@@ -493,8 +493,8 @@ Terimakasih');
 				$message = 'Paket ' . $data_rup['nama_metode_pengadaan']  . ' ' . $data_rup['nama_rup'] . ' Telah diumumkan, silahkan login ke https://eprocurement.jmto.co.id/auth untuk monitoring proses tender berlangsung.';
 				$this->kirim_wa->kirim_wa_vendor_terdaftar($value2['no_telpon'], $message);
 			}
-			// $this->M_panitia->update_rup_panitia($data_rup['id_rup'], $data);
-			// $this->email_send->sen_email_pengumuman($data_rup['id_rup']);
+			$this->M_panitia->update_rup_panitia($data_rup['id_rup'], $data);
+			$this->email_send->sen_email_pengumuman($data_rup['id_rup']);
 
 
 			// $this->email_send->sen_email_finalisasi_panitia($data_rup['id_rup']);
