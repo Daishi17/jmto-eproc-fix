f0ca38207baa4974d7d744012020ece234fa54d0
unknown <danang1724@gmail.com>
2024-05-16 04:34:19 +0700

    update penilaian

--- application/views/administrator/penilaian_kinerja/buat_penilaian.php
+++ application/views/administrator/penilaian_kinerja/buat_penilaian.php
@@ -26,27 +26,33 @@
                 </div>
                 <div class="card-body">
                     <nav>
-                        <div class="nav nav-tabs" id="nav-tab" role="tablist">
-                            <button class="nav-link active" id="nav-home-tab" data-bs-toggle="tab" data-bs-target="#nav-home" type="button" role="tab" aria-controls="nav-home" aria-selected="true">Masa Pelaksanaan</button>
-                            <button class="nav-link" id="nav-profile-tab" data-bs-toggle="tab" data-bs-target="#nav-profile" type="button" role="tab" aria-controls="nav-profile" aria-selected="false">Masa Pemeliharaan</button>
-                        </div>
+                        <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+                            <li class="nav-item" role="presentation">
+                                <button class="nav-link active" id="nav-home-tab" data-bs-toggle="tab" data-bs-target="#nav-home" type="button" role="tab" aria-controls="nav-home" aria-selected="true">Masa Pelaksanaan</button>
+                            </li>
+                            <li class="nav-item" role="presentation">
+                                <button class="nav-link" id="nav-profile-tab" data-bs-toggle="tab" data-bs-target="#nav-profile" type="button" role="tab" aria-controls="nav-profile" aria-selected="false">Masa Pemeliharaan</button>
+                            </li>
+                        </ul>
                     </nav>
                     <div class="tab-content" id="nav-tabContent">
                         <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
                             <br>
                             <ul class="nav nav-tabs" id="myTab" role="tablist">
-                                <li class="nav-item" role="presentation">
-                                    <button class="nav-link active" id="penilaian_1-tab" data-bs-toggle="tab" data-bs-target="#penilaian_1" type="button" role="tab" aria-controls="penilaian_1" aria-selected="true">Masa Pelaksanaan (Satgas)</button>
-                                </li>
-                                <li class="nav-item" role="presentation">
-                                    <button class="nav-link" id="penilaian_2-tab" data-bs-toggle="tab" data-bs-target="#penilaian_2" type="button" role="tab" aria-controls="penilaian_2" aria-selected="false">Masa Pelaksanaan (Manager)</button>
-                                </li>
-                                <li class="nav-item" role="presentation">
-                                    <button class="nav-link" id="penilaian_3-tab" data-bs-toggle="tab" data-bs-target="#penilaian_3" type="button" role="tab" aria-controls="penilaian_3" aria-selected="false">Masa Pelaksanaan (DeptHead)</button>
-                                </li>
-                                <li class="nav-item" role="presentation">
-                                    <button class="nav-link" id="penilaian_4-tab" data-bs-toggle="tab" data-bs-target="#penilaian_4" type="button" role="tab" aria-controls="penilaian_4" aria-selected="false">Rekap Masa Pelaksanaan</button>
-                                </li>
+                                <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+                                    <li class="nav-item" role="presentation">
+                                        <button class="nav-link active" id="penilaian_1-tab" data-bs-toggle="tab" data-bs-target="#penilaian_1" type="button" role="tab" aria-controls="penilaian_1" aria-selected="true">Penilaian Satgas / Area Manager</button>
+                                    </li>
+                                    <li class="nav-item" role="presentation">
+                                        <button class="nav-link" id="penilaian_2-tab" data-bs-toggle="tab" data-bs-target="#penilaian_2" type="button" role="tab" aria-controls="penilaian_2" aria-selected="false">Penilaian Manager / Section Head</button>
+                                    </li>
+                                    <li class="nav-item" role="presentation">
+                                        <button class="nav-link" id="penilaian_3-tab" data-bs-toggle="tab" data-bs-target="#penilaian_3" type="button" role="tab" aria-controls="penilaian_3" aria-selected="false">Penilaian Department Head</button>
+                                    </li>
+                                    <li class="nav-item" role="presentation">
+                                        <button class="nav-link" id="penilaian_4-tab" data-bs-toggle="tab" data-bs-target="#penilaian_4" type="button" role="tab" aria-controls="penilaian_4" aria-selected="false">Rekap Penilaian Kinerja</button>
+                                    </li>
+                                </ul>
                             </ul>
 
                             <div class="tab-content" id="myTabContent">
@@ -57,18 +63,20 @@
                         <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
                             <br>
                             <ul class="nav nav-tabs" id="myTab" role="tablist">
-                                <li class="nav-item" role="presentation">
-                                    <button class="nav-link active" id="penilaian_5-tab" data-bs-toggle="tab" data-bs-target="#penilaian_5" type="button" role="tab" aria-controls="penilaian_5" aria-selected="true">Masa Pemeliharaan (Satgas)</button>
-                                </li>
-                                <li class="nav-item" role="presentation">
-                                    <button class="nav-link" id="penilaian_6-tab" data-bs-toggle="tab" data-bs-target="#penilaian_6" type="button" role="tab" aria-controls="penilaian_6" aria-selected="false">Masa Pemeliharaan (Manager)</button>
-                                </li>
-                                <li class="nav-item" role="presentation">
-                                    <button class="nav-link" id="penilaian_7-tab" data-bs-toggle="tab" data-bs-target="#penilaian_7" type="button" role="tab" aria-controls="penilaian_7" aria-selected="false">Masa Pemeliharaan (DeptHead)</button>
-                                </li>
-                                <li class="nav-item" role="presentation">
-                                    <button class="nav-link" id="penilaian_8-tab" data-bs-toggle="tab" data-bs-target="#penilaian_8" type="button" role="tab" aria-controls="penilaian_8" aria-selected="false">Rekap Masa Pemeliharaan</button>
-                                </li>
+                                <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+                                    <li class="nav-item" role="presentation">
+                                        <button class="nav-link active" id="penilaian_5-tab" data-bs-toggle="tab" data-bs-target="#penilaian_5" type="button" role="tab" aria-controls="penilaian_5" aria-selected="true">Penilaian Satgas / Area Manager</button>
+                                    </li>
+                                    <li class="nav-item" role="presentation">
+                                        <button class="nav-link" id="penilaian_6-tab" data-bs-toggle="tab" data-bs-target="#penilaian_6" type="button" role="tab" aria-controls="penilaian_6" aria-selected="false">Penilaian Manager / Section Head</button>
+                                    </li>
+                                    <li class="nav-item" role="presentation">
+                                        <button class="nav-link" id="penilaian_7-tab" data-bs-toggle="tab" data-bs-target="#penilaian_7" type="button" role="tab" aria-controls="penilaian_7" aria-selected="false">Penilaian Department Head</button>
+                                    </li>
+                                    <li class="nav-item" role="presentation">
+                                        <button class="nav-link" id="penilaian_8-tab" data-bs-toggle="tab" data-bs-target="#penilaian_8" type="button" role="tab" aria-controls="penilaian_8" aria-selected="false">Rekap Penilaian Kinerja</button>
+                                    </li>
+                                </ul>
                             </ul>
 
                             <div class="tab-content">
